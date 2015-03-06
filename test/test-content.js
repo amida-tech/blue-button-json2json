8602361739cdf6b9f7ffab615f22a2c8003e81df
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-03-05 20:37:51 -0500

    testing

--- test/test-content.js
+++ test/test-content.js
@@ -4,14 +4,14 @@ var chai = require('chai');
 
 var json2json = require('../index');
 
-var case_0 = require('./test_cases/case_content_0');
+var case_0 = require('./test_cases/case-content-0');
 
 var expect = chai.expect;
 
 describe('content', function () {
     var engine = json2json.instance();
 
-    it('case_content_0: basic', function () {
+    it('case-content-0: basic', function () {
         var actual = engine.run(case_0.template, case_0.input);
         expect(actual).to.deep.equal(case_0.expected);
     });
