d4b595c1457755dc0367bb9377780263868b1a9e
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-06-25 12:54:39 -0400

    remove unreachable code + more coverage

--- test/test_cases/case-arrayContent-1.js
+++ test/test_cases/case-arrayContent-1.js
@@ -46,12 +46,7 @@ var person = {
 };
 
 exports.template = {
-    arrayContent: [{
-        constant: {
-            title: 'Person & Dog',
-            comment: 'dogs are good'
-        }
-    }, person, cat, dog],
+    arrayContent: [person, cat, dog],
     dataKey: 'team'
 };
 
@@ -60,54 +55,11 @@ exports.expecteds = [];
 
 exports.inputs[0] = {
     team: {
-        person: {
-            name: 'DOE, JOE',
-            birthYear: 1980
-        },
-        dog: {
-            name: 'Paws',
+        donkey: {
+            name: 'uzunkulak',
             birthYear: 2010
         }
     }
 };
 
-exports.expecteds[0] = [{
-    title: 'Person & Dog',
-    comment: 'dogs are good',
-}, {
-    personName: 'DOE, JOE',
-    personAge: 35,
-}, {
-    dogName: 'Paws',
-    dogAge: 5
-}];
-
-exports.inputs[1] = {
-    team: {
-        person: [{
-            name: 'DOE, JOE',
-            birthYear: 1980
-        }, {
-            name: 'DOE, MARY',
-            birthYear: 1984
-        }],
-        dog: {
-            name: 'Paws',
-            birthYear: 2010
-        }
-    }
-};
-
-exports.expecteds[1] = [{
-    title: 'Person & Dog',
-    comment: 'dogs are good',
-}, {
-    personName: 'DOE, JOE',
-    personAge: 35,
-}, {
-    personName: 'DOE, MARY',
-    personAge: 31,
-}, {
-    dogName: 'Paws',
-    dogAge: 5
-}];
+exports.expecteds[0] = null;
